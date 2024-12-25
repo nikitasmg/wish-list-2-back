3ec90803273ab4569361d11b817046f5991d5fd8
nvsmagin <nvsmagin@gid.ru>
2024-12-25 11:06:03 +0500

    feat: Настройки проекта

---
+++ database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"main/model"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *gorm.DB
+
+// функция подключения к базе данных
+func Connect() error {
+	connStr := "user=username dbname=wishListDatabase sslmode=disable password=password"
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	if err != nil {
+		return fmt.Errorf("ошибка подключения к БД: %v", err)
+	}
+
+	DB = db
+	err = db.AutoMigrate(&model.User{}, &model.Present{}, &model.WishList{})
+	if err != nil {
+		return fmt.Errorf("ошибка миграции: %v", err)
+	}
+	log.Println("Успешно подключились к базе данных")
+	return nil
+}
--- main.go
+++ main.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/joho/godotenv"
 	"log"
+	"main/database"
 	"main/router"
 	"os"
 )
 
 func main() {
-	app := fiber.New()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// инициализируем базу данных
+	if err := database.Connect(); err != nil {
+		log.Fatalf("Ошибка подключения к базе данных: %v", err)
+	}
+	app := fiber.New(fiber.Config{
+		Prefork: true,
+	})
+	app.Use(logger.New())
+	app.Use(compress.New())
 	app.Use(cors.New())
+	app.Use(recover.New())
+	app.Use(limiter.New())
 
 	// Get the PORT from heroku env
 	port := os.Getenv("PORT")
